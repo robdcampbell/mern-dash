faae4f869dd28c0bcaa2da2de5849edcacd573ad
robdcampbell <rdeancampbell@gmail.com>
2022-01-07 16:42:56 -0500

    Add React-router and begin flushing out overall file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>MERN Dash</h1>
-      <div>
-        <Landing />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<h1>Error</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,30 +1,29 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components/index.js";
 
 const Landing = () => {
   return (
-    <div>
-      <main>
-        <nav>
-          <img src={logo} alt="MERN-Dash" className="logo" />
-        </nav>
-        <div className="container page">
-          <div className="info">
-            <h1>
-              Job <span>tracking</span> app
-            </h1>
-            <p>
-              I'm baby neutra paleo tilde farm-to-table roof party iPhone twee
-              heirloom locavore af street art dreamcatcher. Next level vice
-              irony waistcoat 8-bit mumblecore gluten-free mixtape mustache
-              tumblr chambray unicorn.
-            </p>
-            <button className="btn btn-hero">Login / Register</button>
-          </div>
-          <img src={main} alt="Landing-Image" className="img main-img" />
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby neutra paleo tilde farm-to-table roof party iPhone twee
+            heirloom locavore af street art dreamcatcher. Next level vice irony
+            waistcoat 8-bit mumblecore gluten-free mixtape mustache tumblr
+            chambray unicorn.
+          </p>
+          <button className="btn btn-hero">Login / Register</button>
         </div>
-      </main>
-    </div>
+        <img src={main} alt="Landing-Image" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
