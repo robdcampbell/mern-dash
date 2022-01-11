8cfe7b82c6d983accf710b46cc614f8fa276b98d
robdcampbell <rdeancampbell@gmail.com>
2022-01-11 16:46:31 -0500

    Begin controllers and routes structure

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+// IMPORTS ////////////////////////
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+// db and authenticate user
 import connectDB from "./db/connect.js";
-
-// Middleware
+// routers import
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+// middleware import
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+// MIDDLEWARE ////////////////////////
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
+// ROUTES ////////////////////////
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+// DB CONNECTION ////////////////////////
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
