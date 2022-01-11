c0f48f53d1cf8f95f3d96b2b2b6df7e49c8f3aa9
robdcampbell <rdeancampbell@gmail.com>
2022-01-11 12:17:53 -0500

    Wireup Clear Alert Functionality with context API

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -13,20 +13,23 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // Global state and useNavigate
-  const { isLoading, showAlert } = useAppContext();
-
-  console.log(isLoading, showAlert);
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+    console.log(values);
   };
 
   return (
