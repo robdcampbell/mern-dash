0fc5eb1f09e72ef0f7fc12a400ce023858b3313b
robdcampbell <rdeancampbell@gmail.com>
2022-01-11 15:05:46 -0500

    Server side setup, connected to MongoAtlas DB

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+import connectDB from "./db/connect.js";
+
+// Middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`Server listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
