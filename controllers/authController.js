960a2210cbb68db3b2073e47f31e39e760015a56
robdcampbell <rdeancampbell@gmail.com>
2022-01-12 16:09:05 -0500

    Initial JWT in place in authController and User.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, NotFoundError } from "../errors/index.js";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -11,8 +11,20 @@ const register = async (req, res, next) => {
   if (userAlreadyExists) {
     throw new BadRequestError("Email already in use");
   }
+  // Mongoose, create new user with UserSchema
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  // JWT creation
+  const token = user.createJWT();
+  // Hardcoding the user detail so the pass doesn't get sent clientside
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+  });
 };
 const login = async (req, res) => {
   res.send("login user");
