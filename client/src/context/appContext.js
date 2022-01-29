9f0e9c1e7d7ac381057ee7450f386dc90a42936d
robdcampbell <rdeancampbell@gmail.com>
2022-01-29 13:27:35 -0500

    State (context, actions, reducer) functionality complete for get all jobs

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -23,6 +23,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -47,6 +49,10 @@ const initialState = {
   jobType: "full-time",
   statusOptions: ["interview", "declined", "pending"],
   status: "pending",
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
 };
 
 const AppContext = React.createContext();
@@ -238,6 +244,25 @@ const AppProvider = ({ children }) => {
       clearAlert();
     }
   };
+
+  const getJobs = async () => {
+    let url = `/jobs`;
+
+    dispatch({ type: GET_JOBS_BEGIN });
+    try {
+      const { data } = await authFetch(url);
+      const { jobs, totalJobs, numOfPages } = data;
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: { jobs, totalJobs, numOfPages },
+      });
+    } catch (error) {
+      console.log(error.response);
+      logOutUser();
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
