2532f8bfcd0aed59b3b742d09e3fc483d4457681
robdcampbell <rdeancampbell@gmail.com>
2022-02-02 06:50:10 -0500

    add limit and skip before pagination in jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -66,6 +66,11 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  const limit = 10;
+  const skip = 1;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
 
   res
