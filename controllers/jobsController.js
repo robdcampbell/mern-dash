8ec9e51da496e2236d59a571b73f9b11f35783cd
robdcampbell <rdeancampbell@gmail.com>
2022-02-02 12:22:25 -0500

    Backend pagination of jobsController done

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -66,16 +66,20 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
-  const limit = 10;
-  const skip = 1;
-
+  // Setup Pagination
+  const page = Number(req.query.page) || 1;
+  // can set this limit in the frontend later!
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
 
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  // total jobs
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
