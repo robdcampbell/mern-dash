1ed5b8078353c3c617b2b00a4930cff7564f3720
robdcampbell <rdeancampbell@gmail.com>
2022-02-03 16:57:03 -0500

    Create Button Container Componenet for jobs pagination/navigation

---
+++ client/src/components/PageBtnContainer.js
@@ -0,0 +1,11 @@
+import { useAppContext } from "../context/appContext";
+
+const PageBtnContainer = () => {
+  return (
+    <div>
+      <h2>Page Btn container</h2>
+    </div>
+  );
+};
+
+export default PageBtnContainer;
