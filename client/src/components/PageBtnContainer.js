a2ffac503a1715f30f089a6424d3888e904d589a
robdcampbell <rdeancampbell@gmail.com>
2022-02-04 16:18:52 -0500

    Pagination in PageBtnContainer.js continued

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,10 +1,29 @@
 import { useAppContext } from "../context/appContext";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
 
 const PageBtnContainer = () => {
+  const { numOfPages, page } = useAppContext();
+
+  const prevPage = () => {
+    console.log("previous page");
+  };
+  const nextPage = () => {
+    console.log("next page");
+  };
+
   return (
-    <div>
-      <h2>Page Btn container</h2>
-    </div>
+    <Wrapper>
+      <button className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">buttons</div>
+      <button className="next-btn">
+        next
+        <HiChevronDoubleRight onClick={nextPage} />
+      </button>
+    </Wrapper>
   );
 };
 
